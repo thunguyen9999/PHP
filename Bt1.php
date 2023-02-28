c94cbf6b4f2c78fd97a35239989d27334c70bc87
Thunguyen9999 <cungdilen1999@gmail.com>
2023-02-28 10:38:06 +0700

    add them file bt1

---
+++ Bt1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$FisrtNumber = "10";
+$SecondNumber = "20";
+$operator = "-";
+switch ($operator) {
+  case "+":
+    echo $FisrtNumber + $SecondNumber;
+    break;
+
+    case "-":
+        echo $FisrtNumber - $SecondNumber;
+        break;
+
+        case "*":
+            echo $FisrtNumber * $SecondNumber;
+            break;
+
+            case "+":
+                echo $FisrtNumber / $SecondNumber;
+                break;
+}
+?>
+ 
+</body>
+</html>
